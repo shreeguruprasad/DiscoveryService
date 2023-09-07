be2dd1ee75534c5b15d4c80db5dc7a1ea20d02ac
shreeguruprasad <nsshreeguruprasad@gmail.com>
2023-09-07 12:16:49 +0530

    iitial commit

---
+++ src/test/java/com/appsdeveloperblog/photoapp/descovery/photoAppDiscoveryService/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.descovery.photoAppDiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
